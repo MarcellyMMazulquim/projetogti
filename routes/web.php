6dc6c3e512cc89bab91cbfa3de4b179e974eaa6b
marcelly.mazulquim@gmail.com <marcelly.mazulquim@gmail.com>
2025-03-20 10:58:46 -0300

    Estudo 20.03

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', function () {
+    return "O código foi testado";
+});
+
+Route::get('/usuario/{nome}', function ($nome) {
+    return "O usuário atual é ".$nome;
+}); 
+
+Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
+    return "a soma é:  ".$num1 + $num2;
+}); 
+
+Route::get('/subtracao/{num1}/{num2}', function ($num1, $num2) {
+    return "a subtração é:  ".$num1 - $num2;
+}); 
+
+Route::get('/multiplicacao/{num1}/{num2}', function ($num1, $num2) {
+    return "a multiplicação é:  ".$num1 * $num2;
+}); 
+
+Route::get('/divisao/{num1}/{num2}', function ($num1, $num2) {
+    return "a divisão é:  ".$num1 / $num2;
+}); 
