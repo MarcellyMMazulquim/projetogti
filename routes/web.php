44df084415430430b3b22af7f1fc5a752cd7d003
MarcellyMMazulquim <marcelly.mazulquim@gmail.com>
2025-04-10 10:45:35 -0300

    Tela de cadastro salvando

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,28 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/criar-conta', function () {
+    return view('criar-conta');
+});
+
+Route::post('/salva-conta', function (Request $request) {
+   //dd($request);
+   $user = new User ();
+   $user->name = $request->name;
+   $user->email = $request->email;
+   $user->password = $request->password;
+   $user->save();
+   
+   return "Usuário salvo com sucesso";
+
+})->name('salva-conta');
 
 Route::get('/teste', function () {
     return "O código foi testado";
@@ -29,3 +47,4 @@ Route::get('/multiplicacao/{num1}/{num2}', function ($num1, $num2) {
 Route::get('/divisao/{num1}/{num2}', function ($num1, $num2) {
     return "a divisão é:  ".$num1 / $num2;
 }); 
+
